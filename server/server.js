25c5fe5692a98d3b1b73d993641bb1ea0c3825c0
Manoj Kumar <manojkumarcse432@gmail.com>
2024-06-21 00:58:46 +0530

    vf-3

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-const PORT = process.env.PORT || 5500;
+
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
