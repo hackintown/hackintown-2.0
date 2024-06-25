8b63f8b3e95829b0abfa33b7658196c0baad1502
manoj mandal <hackintown99@gmail.com>
2024-06-25 16:08:02 +0530

    api-added

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   apiBaseUrl: process.env.NODE_ENV === 'production'
-    ? 'https://hackintown-v2-0-server.onrender.com'
+    ? 'https://hackintown-v2-0-server.onrender.com/api'
     : 'http://localhost:5000'
 };
 
